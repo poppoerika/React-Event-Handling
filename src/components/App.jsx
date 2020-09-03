cd9e833e7e5e0fb0205d722c152fabda459d9081
Erika Tharp <eems0926@gmail.com>
2020-09-02 18:30:33 -0700

    Complete React Event Handler

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [mouseState, setButtonStyle] = useState();
+
+  function changeBBG() {
+    setButtonStyle(true);
+  }
+
+  function changeWBG() {
+    setButtonStyle(false);
+  }
+
+  const [headingText, setHeadingText] = useState("Hello");
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={{ backgroundColor: mouseState ? "black" : "white" }}
+        onMouseOver={changeBBG}
+        onMouseOut={changeWBG}
+      >
+        Submit
+      </button>
     </div>
   );
 }
